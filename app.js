56f8d1ac6256e978d9c53c22ff9ce2738111bb67
slava sapir <slavas21@gmail.com>
2020-02-13 20:10:36 -0500

    some changes

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var mongoose = require('mongoose');
 var dev_db_url = 'mongodb+srv://dbAdmin:dbAdminPass@cluster0-6b2jm.mongodb.net/local_library?retryWrites=true';
 //var mongoDB = process.env.MONGODB_URI || dev_db_url;
 var mongoDB = appEnv.getServiceURL('node-express-tutorial-mongodb') || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true } );
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
